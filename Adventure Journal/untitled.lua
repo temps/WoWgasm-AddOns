ad8eede0e634ef0cf1f88dcc11770596e117d9cb
Temps <oliverkidd94@gmail.com>
2017-07-12 14:57:22 +0100

    Update

---
+++ Adventure Journal/untitled.lua
@@ -0,0 +1,9 @@
+-- IsGMClient Function & Adverture Journal AddOn function
+function IsGMClient(self)
+	if (GetGMLevel) return true then
+		Print = ("GM Access granted")
+		else return false
+		end
+	end
+
+	
